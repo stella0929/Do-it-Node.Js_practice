5bef6e264ab02f1f99d02b9ff298eeaf2fd47268
stella0929 <jang99@ewhain.net>
2020-03-05 01:11:15 +0900

    study Day14

--- ModuleExample/user1.js
+++ ModuleExample/user1.js
@@ -2,8 +2,9 @@
 /* eslint-disable no-console */
 /*eslint no-undef: "error"*/
 /*eslint-env node*/ 
+//exports에 객체 바로 할당할 수 없음. 속성만 넣어줘야함
 exports.getUser = function(){
 	return {id:'test01', name:'소녀시대'};
 };
 
-exports.group = {id:'group01', name:'친구'};
+exports.group = {id:'group01', name:'친구'}; 
